2edda243cb9a1773a460177e46afca1e757f85ea
Romario Sousa <estreladamanha585@gmail.com>
2020-03-12 19:09:33 -0300

    Jafa, kree

---
+++ src/Jafa/kree/main/Jafa.java
@@ -0,0 +1,26 @@
+package Jafa.kree.main;
+
+public class Jafa {
+	
+	private String nome;
+	
+	private String systemLord;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSystemLord() {
+		return systemLord;
+	}
+	public void setSystemLord(String systemLord) {
+		this.systemLord = systemLord;
+	}
+	
+	public void saudar() {
+		System.out.println("Jafa, kree!");	
+	}
+
+}
---
+++ src/LiberdadeProsJafa.java
@@ -0,0 +1,14 @@
+import Jafa.kree.main.Jafa;
+
+public class LiberdadeProsJafa {
+
+	public static void main(String[] args) {
+		System.out.println("Liberta os Jafa, pô");
+		Jafa j = new Jafa();
+		j.saudar();
+
+	}
+	
+	
+
+}
