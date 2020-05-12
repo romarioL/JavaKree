b4f62912ce63f0aadfa38f18de3414e6b6c428dd
Romario Sousa <estreladamanha585@gmail.com>
2020-05-12 19:34:59 -0300

    mothership

---
+++ src/Jafa/kree/main/Mothership.java
@@ -0,0 +1,41 @@
+package Jafa.kree.main;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Mothership {
+	
+	private SystemLord systemLord;
+	
+	private ArrayList<Jafa> jafas = new ArrayList<Jafa>();
+	
+
+	public SystemLord getSystemLord() {
+		return this.systemLord;
+	}
+
+	public void setSystemLord(SystemLord systemLord) {
+		this.systemLord = systemLord;
+	}
+
+	public ArrayList<Jafa> getJafas() {
+		return jafas;
+	}
+
+	public void setJafas(ArrayList<Jafa> jafas) {
+		this.jafas = jafas;
+	}
+	
+	public void addJafa(Jafa j) {
+		jafas.add(j);
+	}
+	
+	public void JafasSaudar() {
+		Iterator<Jafa>  itr = jafas.iterator();
+		while(itr.hasNext()) {
+			Jafa j = itr.next();
+			 System.out.println(j.saudar());
+		}
+	}
+
+}
--- src/LiberdadeProsJafa.java
+++ src/LiberdadeProsJafa.java
@@ -1,4 +1,5 @@
 import Jafa.kree.main.Jafa;
+import Jafa.kree.main.Mothership;
 import Jafa.kree.main.SystemLord;
 
 public class LiberdadeProsJafa {
@@ -7,6 +8,14 @@ public class LiberdadeProsJafa {
 		System.out.println("Liberta os Jafa, pô");
 		Jafa j = new Jafa();
 		j.setNome("Tealc");
+		Jafa j2 = new Jafa();
+		j2.setNome("Bettet");
+		Jafa j3 = new Jafa();
+		j3.setNome("Lucifer");
+		Jafa j4 = new Jafa();
+		j4.setNome("Gay");
+		Jafa j5 = new Jafa();
+		j5.setNome("Dimas");
 		System.out.println(j.saudar());
         System.out.println(j.marchar(4));
         System.out.println(j.marchar(150));
@@ -19,6 +28,17 @@ public class LiberdadeProsJafa {
         System.out.println(s.destroy(100));
         System.out.println(s.orders(j));
         j.setSystemLord(s);
+        
+        Mothership m = new Mothership();
+        m.addJafa(j);
+        m.addJafa(j2);
+        m.addJafa(j3);
+        m.addJafa(j4);
+        m.addJafa(j5);
+        
+        m.JafasSaudar();
+        
+        
         
         
 
