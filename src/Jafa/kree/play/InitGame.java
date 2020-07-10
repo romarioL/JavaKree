c3b7f5e1ec4affd3242f36a0cad1456442148c46
Romis Sousa <estreladamanha585@gmail.com>
2020-07-09 22:37:26 -0300

    Classe de incio de jogo

---
+++ src/Jafa/kree/play/InitGame.java
@@ -0,0 +1,56 @@
+package Jafa.kree.play;
+import java.util.ArrayList;
+
+import Jafa.kree.main.*;
+
+public class InitGame {
+	
+	private ArrayList<Jafa> jafas;
+	private SystemLord systemLord;
+	private ArrayList<Human> humans;
+	private Captain captain;
+	
+
+	public ArrayList<Jafa> getJafas() {
+		return this.jafas;
+	}
+
+	public void setJafa(ArrayList<Jafa> jafas) {
+		this.jafas = jafas;
+	}
+
+	
+	public ArrayList<Human> getHumans() {
+		return this.humans;
+	}
+
+	public void setHumans(ArrayList<Human> humans) {
+		this.humans = humans;
+	}
+
+	public SystemLord getSystemLord() {
+		return this.systemLord;
+	}
+
+	public void setSystemLord(SystemLord systemLord) {
+		this.systemLord = systemLord;
+	}
+
+	public Captain getCaptain() {
+		return this.captain;
+	}
+
+	public void setCaptain(Captain captain) {
+		this.captain = captain;
+	}
+	
+	public String Init(int jafaLife, int humanLife) {
+		Mothership m = new Mothership(systemLord, jafas);
+		SGTeam s = new SGTeam(humans, captain);
+		Game g = new Game(s, m);
+		return g.Play(jafaLife, humanLife);
+	}
+	
+	
+
+}
